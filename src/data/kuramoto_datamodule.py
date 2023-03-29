a2b578849b058456fd560898db40d8eed3ea4380
Jan Lukas Augustin <jan.lukas.augustin@gmail.com>
2023-03-29 13:21:01 +0000

    after inital pre-commit run

--- src/data/kuramoto_datamodule.py
+++ src/data/kuramoto_datamodule.py
@@ -1,14 +1,13 @@
-from typing import Any, Dict, Optional, Tuple
-
-import torch
-from lightning import LightningDataModule
-from torch.utils.data import DataLoader, Dataset
-import numpy as np
-from tqdm import tqdm
 import os
 from os import path as osp
+from typing import Any, Dict, Optional, Tuple
 
+import numpy as np
+import torch
 from kuramoto import Kuramoto
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
 
 
 class KuramotoDataModule(LightningDataModule):
@@ -16,7 +15,7 @@ class KuramotoDataModule(LightningDataModule):
 
     The module is initialized with the parameters that define the dataset.
     If the dataset does not exist, it will be created. Otherwise a dataloader containing the dataset can readily be used.
-    
+
     The dataset parameters include:
     - n_clusters
     - cluster_size
@@ -49,7 +48,7 @@ class KuramotoDataModule(LightningDataModule):
         n_train: int = 300,
         n_val: int = 100,
         n_test: int = 100,
-        seed: int = 0, # Maybe possible to set seed globally elsewhere?
+        seed: int = 0,  # Maybe possible to set seed globally elsewhere?
         batch_size: int = 64,
         num_workers: int = 0,
         data_dir: str = "data/kuramoto",
@@ -62,7 +61,10 @@ class KuramotoDataModule(LightningDataModule):
         self.save_hyperparameters(logger=False)
 
         self.prefix = "{seed}_{n_clusters}_{cluster_size}".format(
-            seed=self.hparams.seed, n_clusters=self.hparams.n_clusters, cluster_size=self.hparams.cluster_size)
+            seed=self.hparams.seed,
+            n_clusters=self.hparams.n_clusters,
+            cluster_size=self.hparams.cluster_size,
+        )
         self.dataset_path = osp.join(self.hparams.data_dir, self.prefix)
 
         self.data_train: Optional[Dataset] = None
@@ -73,59 +75,89 @@ class KuramotoDataModule(LightningDataModule):
         cluster_blocks = []
         class_blocks = []
         for n in range(0, self.hparams.n_clusters):
-            cluster_blocks.append(torch.ones([self.hparams.cluster_size, self.hparams.cluster_size]))
+            cluster_blocks.append(
+                torch.ones([self.hparams.cluster_size, self.hparams.cluster_size])
+            )
             class_blocks.append(torch.ones(self.hparams.cluster_size, dtype=torch.int64) * n)
         single_sample_adj = torch.block_diag(*cluster_blocks)
 
         return single_sample_adj
-    
+
     def create_samples(self):
         single_sample_adj = self.create_adjacency_matrix()
-        nat_freqs = np.random.normal(loc=10, scale=1.0, size=self.hparams.n_clusters * self.hparams.cluster_size)
-        model = Kuramoto(coupling=10, dt=0.001, T=self.hparams.n_timesteps*0.001, n_nodes=len(single_sample_adj.numpy()), natfreqs=nat_freqs)
+        nat_freqs = np.random.normal(
+            loc=10, scale=1.0, size=self.hparams.n_clusters * self.hparams.cluster_size
+        )
+        model = Kuramoto(
+            coupling=10,
+            dt=0.001,
+            T=self.hparams.n_timesteps * 0.001,
+            n_nodes=len(single_sample_adj.numpy()),
+            natfreqs=nat_freqs,
+        )
 
         act_mats_train = []
         for i in tqdm(range(0, self.hparams.n_train)):
-            act_mat = model.run(adj_mat=single_sample_adj.numpy(), angles_vec=np.random.uniform(0, 2*np.pi, size=self.hparams.n_clusters * self.hparams.cluster_size))
+            act_mat = model.run(
+                adj_mat=single_sample_adj.numpy(),
+                angles_vec=np.random.uniform(
+                    0, 2 * np.pi, size=self.hparams.n_clusters * self.hparams.cluster_size
+                ),
+            )
             act_mats_train.append(act_mat)
         act_mats_train_nd = np.vstack(act_mats_train)
         act_mats_train_t = torch.from_numpy(act_mats_train_nd).type(torch.FloatTensor)
 
         act_mats_val = []
         for i in tqdm(range(0, self.hparams.n_val)):
-            act_mat = model.run(adj_mat=single_sample_adj.numpy(), angles_vec=np.random.uniform(0, 2*np.pi, size=self.hparams.n_clusters * self.hparams.cluster_size))
+            act_mat = model.run(
+                adj_mat=single_sample_adj.numpy(),
+                angles_vec=np.random.uniform(
+                    0, 2 * np.pi, size=self.hparams.n_clusters * self.hparams.cluster_size
+                ),
+            )
             act_mats_val.append(act_mat)
         act_mats_val_nd = np.vstack(act_mats_val)
         act_mats_val_t = torch.from_numpy(act_mats_val_nd).type(torch.FloatTensor)
 
         act_mats_test = []
         for i in tqdm(range(0, self.hparams.n_test)):
-            act_mat = model.run(adj_mat=single_sample_adj.numpy(), angles_vec=np.random.uniform(0, 2*np.pi, size=self.hparams.n_clusters * self.hparams.cluster_size))
+            act_mat = model.run(
+                adj_mat=single_sample_adj.numpy(),
+                angles_vec=np.random.uniform(
+                    0, 2 * np.pi, size=self.hparams.n_clusters * self.hparams.cluster_size
+                ),
+            )
             act_mats_test.append(act_mat)
         act_mats_test_nd = np.vstack(act_mats_test)
         act_mats_test_t = torch.from_numpy(act_mats_test_nd).type(torch.FloatTensor)
 
         return single_sample_adj, act_mats_train_t, act_mats_val_t, act_mats_test_t
-    
+
     def save_data(self, single_sample_adj, features_train, features_val, features_test):
         # features
-        torch.save(torch.sin(features_train).clone(), osp.join(self.dataset_path, "features_train.pt"))
+        torch.save(
+            torch.sin(features_train).clone(), osp.join(self.dataset_path, "features_train.pt")
+        )
         torch.save(torch.sin(features_val).clone(), osp.join(self.dataset_path, "features_val.pt"))
-        torch.save(torch.sin(features_test).clone(), osp.join(self.dataset_path, "features_test.pt"))
+        torch.save(
+            torch.sin(features_test).clone(), osp.join(self.dataset_path, "features_test.pt")
+        )
         # adj_original
         torch.save(single_sample_adj, osp.join(self.dataset_path, "original_adj.pt"))
-    
+
     def prepare_data(self):
-        """Generate the dataset if it does not exist.
-        """
+        """Generate the dataset if it does not exist."""
         if not osp.exists(self.dataset_path):
             os.makedirs(self.dataset_path)
             single_sample_adj, features_train, features_val, features_test = self.create_samples()
             self.save_data(single_sample_adj, features_train, features_val, features_test)
-    
+
     def setup(self, stage: Optional[str] = None):
-        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
-        This method is called by lightning with both `trainer.fit()` and `trainer.test()`.
+        """Load data.
+
+        Set variables: `self.data_train`, `self.data_val`, `self.data_test`. This method is called
+        by lightning with both `trainer.fit()` and `trainer.test()`.
         """
         # load and split datasets only if not loaded already
         if not self.data_train and not self.data_val and not self.data_test:
