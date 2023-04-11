successful trials after fixing masking

src/models/components/graph_dae.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn.functional as F
+from torch_geometric.nn.dense import DenseGCNConv
 
 from .graph_generator import FullParam, MLP_Diag, TCNGen
-from .graph_layers import DenseGraphConv, DenseGraphTCNConv
+from .graph_layers import DenseGraphConv, DenseGraphTCNConv, DenseGraphTemporalConv
 from .utils import get_off_diagonal_elements, normalize_adj, symmetrize_adj
 
 
@@ -41,11 +42,29 @@ class GraphDAE(torch.nn.Module):
                 self.layers.append(DenseGraphConv(in_dim, hidden_dim, aggr="add"))
             self.layers.append(DenseGraphConv(hidden_dim, out_dim, aggr="add"))
 
-        elif graph_layer == "GraphTCNConv":
-            self.layers.append(DenseGraphTCNConv(n_channels=n_channels, aggr="add"))
+        elif graph_layer == "GCNConv":
+            self.layers.append(DenseGCNConv(in_dim, hidden_dim))
             for _ in range(n_layers - 2):
-                self.layers.append(DenseGraphTCNConv(n_channels=n_channels, aggr="add"))
-            self.layers.append(DenseGraphTCNConv(n_channels=n_channels, aggr="add"))
+                self.layers.append(DenseGCNConv(hidden_dim, hidden_dim))
+            self.layers.append(DenseGCNConv(hidden_dim, out_dim))
+
+        elif graph_layer == "GraphTemporalConv":
+            self.layers.append(
+                DenseGraphTemporalConv(in_dim, hidden_dim, aggr="add", dilation_size=2)
+            )
+            self.layers.append(
+                DenseGraphTemporalConv(hidden_dim, hidden_dim, aggr="add", dilation_size=4)
+            )
+            self.layers.append(
+                DenseGraphTemporalConv(hidden_dim, out_dim, aggr="add", dilation_size=8)
+            )
+            self.layers.append(torch.nn.Linear(out_dim, out_dim))
+
+        elif graph_layer == "GraphTCNConv":
+            self.layers.append(DenseGraphTCNConv(n_channels=n_channels, aggr="mean"))
+            for _ in range(n_layers - 2):
+                self.layers.append(DenseGraphTCNConv(n_channels=n_channels, aggr="mean"))
+            self.layers.append(DenseGraphTCNConv(n_channels=n_channels, aggr="mean"))
 
         if gen_mode == "FP":
             self.graph_gen = FullParam(
@@ -84,9 +103,14 @@ class GraphDAE(torch.nn.Module):
         Adj_ = self.get_adj(x)
         Adj = F.dropout(Adj_, p=self.dropout_adj)
         # Adj, Adj_ = torch.ones((8, 8)), torch.ones((8, 8))
+        identity = noisy_x
         for i, conv in enumerate(self.layers[:-1]):
-            denoised_x = conv(noisy_x, Adj)
-            denoised_x = F.relu(denoised_x)
-            denoised_x = F.dropout(denoised_x, p=self.dropout_dae)
-        denoised_x = self.layers[-1](denoised_x, Adj)  # no dropout on last layer
-        return denoised_x, Adj_
+            # identity = noisy_x
+            noisy_x = conv(noisy_x, Adj)  # , add_loop=False)
+            # noisy_x = noisy_x + identity
+            noisy_x = F.relu(noisy_x)
+            noisy_x = F.dropout(noisy_x, p=self.dropout_dae)
+            noisy_x = identity + noisy_x  # make residual connection option?
+        # add option to add residual connection to last layer
+        noisy_x = self.layers[-1](noisy_x, Adj)  # , add_loop=False)  # no dropout on last layer
+        return noisy_x, Adj_

src/models/components/graph_generator.py
@@ -25,7 +25,7 @@ class FullParam(nn.Module):
         #    Adj = torch.exp(self.Adj)
         # elif self.gen_act == "elu":
         #    Adj = F.elu(self.Adj) + 1
-        return torch.sigmoid(self.Adj)  # F.elu(self.Adj) + 1
+        return F.elu(self.Adj)
 
 
 class MLP_Diag(nn.Module):

src/models/cp4sl_module.py
@@ -78,24 +78,16 @@ class CP4SLLitModule(LightningModule):
         self.val_recon_error_best.reset()
         self.val_graph_error_best.reset()
 
-    def get_random_mask(self, x):
-        nones = torch.sum(x > 0.0).float()
-        nzeros = x.shape[0] * x.shape[1] - nones
-        pzeros = nones / nzeros / self.hparams.ratio * self.hparams.nr
-        probs = torch.zeros(x.shape)
-        probs[x == 0.0] = pzeros
-        probs[x > 0.0] = 1 / self.hparams.ratio
-        mask = torch.bernoulli(probs)
-        return mask
-
     def model_step(self, batch: Any):
         x, original_adj = batch
-        mask = self.get_random_mask(x)
+        mask = torch.bernoulli(torch.ones_like(x) * 0.1)
         # apply noise
         if self.hparams.noise == "mask":
-            noisy_x = x * (1 - mask)
+            noisy_x = x
+            noisy_x[mask > 0] = 0.0
+            # noisy_x = x * (1 - mask)
         elif self.hparams.noise == "normal":
-            noise = torch.normal(0.0, 0.1, size=x.shape)
+            noise = torch.normal(0.0, 1.0, size=x.shape)
             noisy_x = x + (noise * mask)
         denoised_x, adj = self.forward(x, noisy_x)
         indices = mask > 0
@@ -169,6 +161,13 @@ class CP4SLLitModule(LightningModule):
             else:
                 plt.imshow(np.median(full_adj.detach().numpy(), axis=0))
             plt.savefig("plots/" + str(time()) + "_val.png")
+
+            plt.clf()
+            plt.plot(x[0, 0, :].cpu().detach().numpy())
+            plt.plot(denoised_x[0, 0, :].cpu().detach().numpy())
+            plt.plot(x[0, 5, :].cpu().detach().numpy())
+            plt.plot(denoised_x[0, 5, :].cpu().detach().numpy())
+            plt.savefig("plots/" + str(time()) + "_x_denoised_x.png")
 
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log(