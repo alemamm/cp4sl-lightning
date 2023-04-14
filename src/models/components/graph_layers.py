5f3097d9a1fb04441e9021a175a56d7810be6000
Jan Lukas Augustin <jan.lukas.augustin@gmail.com>
2023-04-14 12:23:50 +0000

    remove temporalconv graph layer

--- src/models/components/graph_layers.py
+++ src/models/components/graph_layers.py
@@ -2,20 +2,8 @@ from typing import List, Optional
 
 import torch
 import torch.nn as nn
-from torch_geometric.nn import inits
 
-from .tcn import TemporalBlock, TemporalConvNet
-
-
-class Diag(nn.Module):
-    def __init__(self, input_size):
-        super().__init__()
-        self.W = nn.Parameter(torch.ones(input_size))
-        self.input_size = input_size
-
-    def forward(self, input):
-        hidden = input @ torch.diag(self.W)
-        return hidden
+from .tcn import TemporalConvNet
 
 
 class TCN(nn.Module):
@@ -81,7 +69,6 @@ class DenseGraphTCNConv(torch.nn.Module):
         else:
             raise NotImplementedError
 
-        # out = x + self.tcn_rel(out.view(-1, C).unsqueeze(2)).view(B, N, C)
         out = self.tcn_rel(out.view(-1, C).unsqueeze(2)).view(B, N, C)
 
         if mask is not None:
@@ -153,7 +140,6 @@ class DenseGraphConv(torch.nn.Module):
         else:
             raise NotImplementedError
 
-        # out = x + self.lin_rel(out)
         out = self.lin_rel(out)
 
         if mask is not None:
@@ -163,77 +149,3 @@ class DenseGraphConv(torch.nn.Module):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.in_channels}, " f"{self.out_channels})"
-
-
-class DenseGraphTemporalConv(torch.nn.Module):
-    r"""See :class:`torch_geometric.nn.conv.GraphConv`."""
-
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: int = 7,
-        dilation_size: int = 2,
-        dropout: float = 0.0,
-        aggr: str = "add",
-    ):
-        assert aggr in ["add", "mean", "max"]
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.aggr = aggr
-
-        self.temp_block = TemporalBlock(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride=1,
-            dilation=dilation_size,
-            padding=(kernel_size - 1) * dilation_size,
-            dropout=dropout,
-        )
-
-    def forward(
-        self, x: torch.Tensor, adj: torch.Tensor, mask: Optional[torch.Tensor] = None
-    ) -> torch.Tensor:
-        r"""
-        Args:
-            x (torch.Tensor): Node feature tensor
-                :math:`\mathbf{X} \in \mathbb{R}^{B \times N \times F}`, with
-                batch-size :math:`B`, (maximum) number of nodes :math:`N` for
-                each graph, and feature dimension :math:`F`.
-            adj (torch.Tensor): Adjacency tensor
-                :math:`\mathbf{A} \in \mathbb{R}^{B \times N \times N}`.
-                The adjacency tensor is broadcastable in the batch dimension,
-                resulting in a shared adjacency matrix for the complete batch.
-            mask (torch.Tensor, optional): Mask matrix
-                :math:`\mathbf{M} \in {\{ 0, 1 \}}^{B \times N}` indicating
-                the valid nodes for each graph. (default: :obj:`None`)
-        """
-        x = x.unsqueeze(0) if x.dim() == 2 else x
-        adj = adj.unsqueeze(0) if adj.dim() == 2 else adj
-        B, N, C = x.size()
-
-        if self.aggr == "add":
-            out = torch.matmul(adj, x)
-        elif self.aggr == "mean":
-            out = torch.matmul(adj, x)
-            out = out / adj.sum(dim=-1, keepdim=True).clamp_(min=1)
-        elif self.aggr == "max":
-            out = x.unsqueeze(-2).repeat(1, 1, N, 1)
-            adj = adj.unsqueeze(-1).expand(B, N, N, C)
-            out[adj == 0] = float("-inf")
-            out = out.max(dim=-3)[0]
-            out[out == float("-inf")] = 0.0
-        else:
-            raise NotImplementedError
-
-        out = self.temp_block(out.view(-1, C).unsqueeze(2)).view(B, N, C)
-
-        if mask is not None:
-            out = out * mask.view(-1, N, 1).to(x.dtype)
-
-        return out
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.n_channels})"
